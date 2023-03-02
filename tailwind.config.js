4f2a6a718b2d751356c88bcf9df5cdfb1f5505aa
RafaelSantos <rafaelpg63@gmail.com>
2023-03-02 12:26:15 -0300

    Including Experience page
    Incluindo toda pagina de experiencia com responsividade mobile,tablet e desktop conforme layout

---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme:{
+    extend:{
+        colors:{
+            roxo:{
+                // Cor primaria
+                escuro:'#4650BD',
+                claro:'#B6BCFF',
+                hover:'#606ADA',
+
+            },
+            verde:{
+                // BG datas
+                escuro:'#008364',
+                // Secundaria
+                secundaria:'#304968',
+                // Tercearia
+                tercearia:'#36c1a0',
+
+                // Gradiente background
+                gradiente:'linear-gradient(180deg, #304968 0%, #36C1A0 52.6%, #4650BD 100%);'
+            },
+            gradiente:{
+                inicio:'#304968',
+                meio:'#36C1A0',
+                fim:'#4650BD',
+            }
+        },
+        fontFamily:{
+            titulo:['Calistoga','sans-serif'],
+            texto:['Raleway','sans-serif']
+        },
+        backgroundImage:{
+            'borealmobile':"url('../img/BorealMobile.svg')",
+            'borealtablet':"url('../img/BorealTablet.svg')",
+            'experiencia':"linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.2)), url('../img/experiencia.jpeg')",
+       
+            'guitar':"url('../img/guitar.svg')",
+            'guitardesktop':"url('../img/guitarDesktop.jpg')",
+            'exp1':"url('../img/exp1.jpeg')",
+            'exp2':"url('../img/exp2.jpeg')",
+            'exp3':"url('../img/exp3.jpeg')",
+
+        },
+        dropShadow:{
+            'botao': '4px 4px 0px #444444'
+        },
+        fontSize:{
+            '32xl': '2rem',
+            '40xl': '2.5rem',
+            '64xl': '4rem',
+        },
+        height:{
+            '110':'28rem',
+            '400':'29rem',
+        }
+    }
+  },
+  plugins: [],
+}
